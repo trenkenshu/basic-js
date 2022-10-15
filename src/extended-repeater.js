e7079841d98801aa5b59f674440db14ff1ecb3a2
trenkenshu <trenkenshu@gmail.com>
2022-10-15 13:58:32 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(string, obj) {
+    let answer = '';
+    let str = String(string);
+    let opt = {
+        rep:     obj.repeatTimes ? obj.repeatTimes : 1,
+        sep:     obj.separator ? obj.separator : '+',
+        add:     typeof obj.addition !== 'undefined' ? String(obj.addition) : false,
+        addSep:  obj.additionSeparator ? obj.additionSeparator : '|',
+        addRep:  obj.additionRepeatTimes ? obj.additionRepeatTimes : 1
+    };
+
+    //c(opt);
+
+    for (let iter = 1; iter <= opt.rep; iter++) {
+        answer += str;
+        if(opt.add && opt.addRep) {
+            for(let ii = 1; ii <= opt.addRep; ii++) {
+                answer += ii === 1 ? opt.add : opt.addSep + opt.add;
+            }
+        }
+        
+        answer += iter < opt.rep ? opt.sep : ''; 
+        
+    }
+    
+    return answer;
 }
 
 module.exports = {
