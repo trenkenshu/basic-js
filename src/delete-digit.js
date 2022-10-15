b8074346cbb339267aa80fdac7ab3c3b535f7142
trenkenshu <trenkenshu@gmail.com>
2022-10-15 19:44:52 +0300

    feat: delete-digit and partial vigenere

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function deleteDigit(n) {
+    let nArr = n.toString().split('');
+    let max = 0;
+    let initialLength = nArr.length;
+
+    for(let i = 0; i < initialLength; i++ ) {
+        
+        let temp = nArr.slice();
+        let tempString = '';
+        temp.splice(i, 1);
+        for(let t = 0; t < temp.length; t++) {
+            tempString += temp[t];
+        } 
+        max = max <= Number(tempString) ? Number(tempString) : max;
+    }
+
+    return max;
+    
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,145 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+    encrypt(string, key) {
+        if( typeof string === "undefined" || typeof key === "undefined") {
+            throw new Error('Incorrect arguments!');
+        }
+        
+        let ans = '';
+        let keyIndex = 0;
+        let ascii = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        string =string.toUpperCase();
+        key = key.toUpperCase();
+            
+        
+        /********************************** NO SPACE  *******************************/
+        let noSpaceArr = string.split(' ');
+        let noSpaceString = '';
+        for (let i in noSpaceArr ) {
+            noSpaceString += noSpaceArr[i];
+        }
+        if(string.length > key.length) {
+           let mult =  Math.floor(string.length / key.length) ;
+           let oldKey = key;
+           while ( mult > 0 ) {
+            key += oldKey;
+            mult--;
+           }
+        
+        }
+
+        for(let i = 0; i < noSpaceString.length; i++) {
+            
+            if ((/[a-zA-Z]/).test(noSpaceString[i])) {
+                let charPos = ascii.indexOf(noSpaceString[i]);
+                let keyPos = ascii.indexOf(key[keyIndex]);
+                let encodedAlpha = ascii.slice(keyPos, 26) + ascii.slice(0, keyPos);
+                let encChar = encodedAlpha[charPos];
+                ans += encChar;
+                keyIndex++;
+            } else {
+                ans += noSpaceString[i];
+                keyIndex = 0;
+            }
+        }
+        let final ='';
+        let posInAns = 0;
+        for(let i = 0; i <= noSpaceArr.length - 1; i++) {
+            final += ans.slice(posInAns, noSpaceArr[i].length + posInAns);
+            final += i < noSpaceArr.length - 1 ? ' ' : '';
+            posInAns += noSpaceArr[i].length;
+        }
+
+        if (this.direction === 'reverse') {
+            let newFinal = '';
+            for(let i = final.length -1; i >= 0; i--) {
+                newFinal += final[i];
+            }
+            final = newFinal;
+        }
+        
+        return final;
+    }
+    /******************************************** decrypt ************************************/
+    decrypt(string, key) {
+        if( typeof string === "undefined" || typeof key === "undefined") {
+            throw new Error('Incorrect arguments!');
+        }
+        
+        let ans;
+        let final = '';
+        let ascii = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        string =string.toUpperCase();
+        key = key.toUpperCase();
+        
+        /********************************** NO SPACE  *******************************/
+        let noSpaceArr = string.split(' ');
+        let noSpaceString = '';
+        for (let i in noSpaceArr ) {
+            noSpaceString += noSpaceArr[i];
+        }
+       
+
+        if(string.length > key.length) {
+           let mult =  Math.floor(string.length / key.length) ;
+           let oldKey = key;
+           while ( mult > 0 ) {
+            key += oldKey;
+            mult--;
+           }
+        
+        }
+        
+        let keyArr = Array.from(key);
+        keyArr.forEach((item, i, arr) => {
+            arr[i] = ascii.indexOf(item);
+            
+        });
+        let stringArr = Array.from(noSpaceString);
+        stringArr.forEach((item, i, arr) => {
+            arr[i] = ascii.indexOf(item);
+        } );
+
+               
+        for(let i = 0; i < stringArr.length; i++) {
+             
+            ans = stringArr[i] - keyArr[i];
+            ans = ans < 0 ? ans + 26 : ans;
+
+            stringArr[i] === -1 ? 
+                final += noSpaceString[i]
+                :
+                final += ascii[ans];
+        }
+
+        let final_ ='';
+        let posInAns = 0;
+        for(let i = 0; i <= noSpaceArr.length - 1; i++) {
+            final_ += final.slice(posInAns, noSpaceArr[i].length + posInAns);
+            final_ += i < noSpaceArr.length - 1 ? ' ' : '';
+            posInAns += noSpaceArr[i].length;
+        }
+
+
+        if (this.direction === 'reverse') {
+            let newFinal = '';
+            for(let i = final_.length -1; i >= 0; i--) {
+                newFinal += final_[i];
+            }
+            final_ = newFinal;
+        }        
+
+        return final_;
+      
+    }
+    
+    constructor(dir) {
+        this.direction = dir === false ? 'reverse' : 'direct';
+    }
+    
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
 module.exports = {
   VigenereCipheringMachine
